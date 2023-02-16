91d208c485cbeb18667b0b29b7202cf0e0cc8d2b
Demetre Panjakidze <Dpanj2021@agruni.edu.ge>
2023-02-17 02:10:00 +0400

    initial commit

--- src/app/homework1/components/rate-window/rate-window.component.ts
+++ src/app/homework1/components/rate-window/rate-window.component.ts
@@ -6,16 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./rate-window.component.scss'],
 })
 export class RateWindowComponent {
-  white: string =
-    'invert(63%) sepia(40%) saturate(0%) hue-rotate(186deg) brightness(99%) contrast(93%)';
-  blue: string =
-    'invert(62%) sepia(24%) saturate(5489%) hue-rotate(190deg) brightness(98%) contrast(91%)';
   buttonColor: string = 'rgba(255, 255, 255, 0.1)';
   textColor: string = 'rgba(255, 255, 255, 0.5)';
-  starColor: string = this.white;
-  starLink: string = 'assets/vote-star.svg';
-  voteNumber: number = 1;
+  starColor: string =
+    'invert(63%) sepia(40%) saturate(0%) hue-rotate(186deg) brightness(99%) contrast(93%)';
+  voteNumber: number = 10;
   fontSize: string = '';
+  starWidth: number = 85;
+  starHeight: number = 81;
+  starPosition: number = -47;
   clicked: boolean = false;
 
   ngAfterViewInit() {
@@ -24,17 +23,23 @@ export class RateWindowComponent {
 
   rating(rate: number) {
     this.clicked = true;
-    this.starColor = this.blue;
-    this.starLink = 'assets/big-star.svg';
+    this.starColor =
+      'invert(62%) sepia(24%) saturate(5489%) hue-rotate(190deg) brightness(98%) contrast(91%)';
     this.buttonColor = '#f5c518';
     this.textColor = '#000';
     this.voteNumber = rate;
     let fontSizeNumber = 22;
-    fontSizeNumber += rate / 2;
+    this.starWidth = 85;
+    this.starHeight = 81;
+    this.starPosition = -47;
+    fontSizeNumber += rate;
+    this.starWidth += (3 * rate) / 2;
+    this.starHeight += (3 * rate) / 2;
+    this.starPosition -= rate / 2;
     this.fontSize = `${fontSizeNumber}px`;
   }
 
-  shouldHighlightStar(voteNumber: number): boolean {
-    return voteNumber <= this.selectedRating;
+  shouldHighlightStar(selectedRating: number): boolean {
+    return selectedRating <= this.voteNumber;
   }
 }
