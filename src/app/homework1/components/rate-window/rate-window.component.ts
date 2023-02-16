1c107c6b7e52b96bec5a42ee32ba5aa9c99139e5
Demetre Panjakidze <Dpanj2021@agruni.edu.ge>
2023-02-17 00:45:25 +0400

    initial commit

--- src/app/homework1/components/rate-window/rate-window.component.ts
+++ src/app/homework1/components/rate-window/rate-window.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-rate-window',
   templateUrl: './rate-window.component.html',
-  styleUrls: ['./rate-window.component.scss']
+  styleUrls: ['./rate-window.component.scss'],
 })
 export class RateWindowComponent {
+  white: string =
+    'invert(63%) sepia(40%) saturate(0%) hue-rotate(186deg) brightness(99%) contrast(93%)';
+  blue: string =
+    'invert(62%) sepia(24%) saturate(5489%) hue-rotate(190deg) brightness(98%) contrast(91%)';
+  ngAfterViewInit() {
+    document.body.style.overflow = 'hidden';
+  }
 
+  rating(rate: number) {
+    console.log(rate);
+  }
 }
